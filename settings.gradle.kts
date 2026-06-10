ad4d8162eb80bdbff000ba62912326f246a9f83b
petem903 <33722411+petem903@users.noreply.github.com>
2026-06-10 15:50:57 -0400

    Fix public Gradle repository configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,6 @@
-val useLocalMirror = System.getenv("HEATMAP_USE_MIRROR") == "1"
-
 pluginManagement {
     repositories {
-        if (useLocalMirror) {
+        if (System.getenv("HEATMAP_USE_MIRROR") == "1") {
             maven { url = uri("http://127.0.0.1:8765/google/");  isAllowInsecureProtocol = true }
             maven { url = uri("http://127.0.0.1:8765/plugins/"); isAllowInsecureProtocol = true }
             maven { url = uri("http://127.0.0.1:8765/central/"); isAllowInsecureProtocol = true }
@@ -17,7 +15,7 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        if (useLocalMirror) {
+        if (System.getenv("HEATMAP_USE_MIRROR") == "1") {
             maven { url = uri("http://127.0.0.1:8765/google/");  isAllowInsecureProtocol = true }
             maven { url = uri("http://127.0.0.1:8765/central/"); isAllowInsecureProtocol = true }
             maven { url = uri("http://127.0.0.1:8765/plugins/"); isAllowInsecureProtocol = true }
